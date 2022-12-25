27f5b3c6098cc7b18977e39e7b7feccd6a4c4ca6
Alex <a.buyanov@activevideo.com>
2022-12-25 10:38:01 +0100

    Init commit

---
+++ building_rest_api/src/main/java/com/springtutorials/buildrest/BuildRestApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorials.buildrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildRestApplication.class, args);
+	}
+
+}
