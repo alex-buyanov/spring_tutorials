a5efe5dc961d88f00324c9529424f3c5480c7708
Alex <a.buyanov@activevideo.com>
2022-12-26 10:30:27 +0100

    Completed task

---
+++ data_access_w_jpa/src/test/java/data_access_w_jpa/DataAccessWJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package data_access_w_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessWJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
