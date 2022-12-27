10c19023418c6f3919831be34ad5d7652f06ae9e
Alexander Buyanov <34279442+alex-buyanov@users.noreply.github.com>
2022-12-27 09:36:26 +0100

    Finished JPA-REST tutorial (#5)
    Co-authored-by: Alex <a.buyanov@activevideo.com>

---
+++ jpa-data-access-rest/src/test/java/spring/jpadataaccessrest/JpaDataAccessRestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jpadataaccessrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDataAccessRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
