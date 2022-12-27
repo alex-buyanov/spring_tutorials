04d416d60f04bcee902620167f42d84053b85d9a
Alex <a.buyanov@activevideo.com>
2022-12-27 11:33:47 +0100

    Finished integration tutorial

---
+++ integration/src/main/java/spring/integration/IntegrationApplication.java
@@ -0,0 +1,20 @@
+package spring.integration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("/integration/integration.xml")
+public class IntegrationApplication {
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext ctx = new SpringApplication(IntegrationApplication.class).run(args);
+		System.out.println("Hit <Enter> to terminate");
+		System.in.read();
+		ctx.close();
+	}
+
+}
