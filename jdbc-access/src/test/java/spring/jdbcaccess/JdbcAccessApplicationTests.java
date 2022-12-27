a7752d226970b879a751c1c39d4fe5885fc0eace
Alexander Buyanov <34279442+alex-buyanov@users.noreply.github.com>
2022-12-27 09:37:00 +0100

    Finished JDBC API tutorial (#6)
    Co-authored-by: Alex <a.buyanov@activevideo.com>

---
+++ jdbc-access/src/test/java/spring/jdbcaccess/JdbcAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jdbcaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
