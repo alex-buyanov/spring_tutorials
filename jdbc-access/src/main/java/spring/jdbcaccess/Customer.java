f2ee59f2fcfa8b729a821eb3a403c5051088fdf8
Alex <a.buyanov@activevideo.com>
2022-12-27 09:33:44 +0100

    Finished JDBC API tutorial

---
+++ jdbc-access/src/main/java/spring/jdbcaccess/Customer.java
@@ -0,0 +1,40 @@
+package spring.jdbcaccess;
+
+public class Customer {
+    private final long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
